64edc66b67d9e0980efa3c4b4403c1bd0e954e99
Camden <CyanCoding@users.noreply.github.com>
2020-10-11 04:47:09 -0400

    Update main.cs
    Added comments

--- main.cs
+++ main.cs
@@ -11,16 +11,21 @@ using System.IO;
 
 class MainClass {
     static void Main(string[] args) {
+        // Read from the file
         string[] quotesFile = File.ReadAllLines("quotes.json");
 
+        // Randomly pick a quote (line from the file)
         Random random = new Random();
         string line = quotesFile[random.Next(quotesFile.Length)];
 
+        // Split the line into the quote and author.
         string[] lineSplit = line.Split("\", \"");
 
+        // Perform text splitting to remove excess leftover characters from split.
         string quote = lineSplit[0].Substring(2);
         string author = lineSplit[1].Substring(0, lineSplit[1].Length - 2);
 
+        // Print output
         Console.WriteLine(quote);
         Console.WriteLine(author);
     }
